37a603930ff089b0647a6e4f84a3a9113c689e22
fsousaentre <85347233+fredsons@users.noreply.github.com>
2022-09-23 18:04:13 -0300

    finaliza projeto

--- script/main.js
+++ script/main.js
@@ -1,5 +1,12 @@
-function tocaSom(idElementoAudio) {
-  document.querySelector(idElementoAudio).play();
+function tocaSom(seletortoAudio) {
+  const elemento = document.querySelector(seletortoAudio);
+
+  if (elemento && elemento.localName === 'audio') {
+    elemento.play();
+  }
+  else {
+    alert("Elemento não encontrado")
+  }
 }
 
 const listaDeTeclas = document.querySelectorAll(".tecla");
@@ -12,4 +19,17 @@ for (let contador = 0; contador < listaDeTeclas.length; contador++) {
   tecla.onclick = function () {
     tocaSom(idAudio);
   };
+
+  tecla.onkeydown = function (evento) {
+
+    if (evento.code === 'Space' || evento.code === 'Enter') {
+      tecla.classList.add('ativa');
+    }
+
+  }
+
+  tecla.onkeyup = function () {
+    tecla.classList.remove('ativa');
+  }
 }
+
